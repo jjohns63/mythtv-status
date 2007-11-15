055b8ede41d145cb51e3de62f756221ccf3e81fe
Francois Marier <francois@debian.org>
2007-11-16 12:19:39 +1300

    Check if the initscript exists before running it

--- debian/cron.d
+++ debian/cron.d
@@ -1,4 +1,4 @@
 #
 # Regular cron jobs for the mythtv-status package
 #
-*/10 *	* * *	root	/etc/init.d/mythtv-status reload > /dev/null
+*/10 *	* * *	root	[ -x /etc/init.d/mythtv-status ] && /etc/init.d/mythtv-status reload > /dev/null
